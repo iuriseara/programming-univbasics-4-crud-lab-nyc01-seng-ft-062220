114892a3c63df898d9e278048ea985a0f6542648
fearless-command-2358 <iuriseara@gmail.com>
2020-05-28 19:23:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["element1", "element2", "element3", "element4"]
-  element = "#{element}"
-  array << "#{element}"
-  puts array
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
