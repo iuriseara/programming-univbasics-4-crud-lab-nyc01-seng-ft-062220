26ecdd3a185cb1c6c1098df6502fbd8b30f667b4
fearless-command-2358 <iuriseara@gmail.com>
2020-05-28 18:50:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,39 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  new_array = ["element1", "element2", "element3", "element4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  new_array = ["element1", "element2", "element3", "element4"]
+
+  new_array.push("new_element")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
